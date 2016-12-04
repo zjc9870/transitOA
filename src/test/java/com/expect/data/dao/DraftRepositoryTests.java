9bb36ce932e9639367147eafbaeb14b86812889c
track-123 <1005549421@qq.com>
2016-12-04 11:45:19 +0800

    test

---
+++ src/test/java/com/expect/data/dao/DraftRepositoryTests.java
@@ -0,0 +1,43 @@
+package com.expect.data.dao;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.expect.admin.data.dao.DraftRepository;
+import com.expect.admin.data.dataobject.Draft;
+import com.expect.util.BaseTest;
+
+public class DraftRepositoryTests extends BaseTest {
+	
+	@Autowired
+	DraftRepository draftRepository;
+
+//	@Test
+	@Ignore
+	public void saveTest(){
+		Assert.assertEquals(0, draftRepository.count());
+		Draft draft = new Draft();
+		draft.setBh("2222222");
+		draft.setSfjtng("Y");
+		draft.setNgshzt("1");
+		draftRepository.save(draft);
+		Assert.assertEquals(1, draftRepository.count());
+	}
+	
+//	@Test
+	@Ignore
+	public void updateTest(){
+		Draft d = draftRepository.findAll().get(0);
+		d.setBmfzrhgyj("sssssssssssssssssss");
+		draftRepository.save(d);
+		Assert.assertEquals(1, draftRepository.count());
+	}
+	
+	@Test
+//	@Ignore
+	public void findTest(){
+		Assert.assertEquals(1, draftRepository.findBySfjtngAndNgshzt("Y", "1").size());
+	}
+}
---
+++ src/test/java/com/expect/util/BaseTest.java
@@ -0,0 +1,16 @@
+package com.expect.util;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.expect.admin.SpringBootCommonApplication;
+
+@SuppressWarnings("deprecation")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootCommonApplication.class)
+@WebAppConfiguration
+public class BaseTest {
+
+}
