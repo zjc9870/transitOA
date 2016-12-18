37f5e08b6245b7048abe34de8eca8bfeb3f03918
LonelyObserver <478528180@qq.com>
2016-12-18 15:31:19 +0800

    合同页面

--- src/main/resources/static/js/admin/contract/c_apply_record.js
+++ src/main/resources/static/js/admin/contract/c_apply_record.js
@@ -1,16 +1,16 @@
 var tabs = document.getElementById('tab').getElementsByTagName('button');
 for(var i=0; i<tabs.length; i++) {
     tabs[i].onclick = function () {
-        AjaxTool.post('contract/test31', {
+        AjaxTool.get('contract/sqjl', {
                 lx: this.id},function (data) {
                 if(data.success) {
                     var str = "<tr>";
                     var cons = data.content;
                     for(var i=0;i<cons.length;i++) {
                         str += "<td>"+cons[i].htbt+"</td>";
-                        str += "<td>"+cons[i].htbt+"</td>";
-                        str += "<td>"+cons[i].htbt+"</td>";
-                        str += "<td>"+cons[i].htbt+"</td>";
+                        str += "<td>"+cons[i].sqsj+"</td>";
+                        str += "<td>"+cons[i].htshzt+"</td>";
+                        str += "<td><div  onclick='seeApplyRecord('"+ cons[i].id +"')'>查看<div>提交</div>删除</div></td>";
                     }
                     str += "</tr>";
                     $('#c-approve-tbody').html(str);
@@ -24,4 +24,8 @@ for(var i=0; i<tabs.length; i++) {
             }
         }
     }
+}
+
+function seeApplyRecord(id) {
+    AjaxTool.post('contract/sqjlxq',{id: id});
 }
