bd209fd852bb46108441d7ee26f7714b39d90bb3
LonelyObserver <478528180@qq.com>
2016-12-19 16:01:28 +0800

    合同页面

--- src/main/resources/static/js/admin/contract/c_backfillDetail.js
+++ src/main/resources/static/js/admin/contract/c_backfillDetail.js
@@ -1,6 +1,7 @@
 function submitForm() {
-    AjaxTool.post('contract/bhht', $('#c_approveForm').serialize(), function (data) {
+    AjaxTool.post('contract/bhht', $('#c_backfillForm').serialize(), function (data) {
             alert(data.message);
         }
     )
 }
+
