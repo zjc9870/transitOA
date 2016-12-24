47b275d1fc614ec9943fd58db5300f0ca584858e
track-123 <1005549421@qq.com>
2016-12-24 18:57:35 +0800

    个人信息设置修改

--- src/main/java/com/expect/admin/web/IndividualController.java
+++ src/main/java/com/expect/admin/web/IndividualController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -44,9 +45,10 @@ public class IndividualController {
 	
 	@PostMapping("/updateIndividualMessage")
 	@ResponseBody 
-	public DataTableRowVo updateIndividualMessage(UserVo userVo, HttpServletResponse response) throws IOException{
-		if(!StringUtil.isBlank(userVo.getAvatarId()))
-			userService.updateAvatar(userVo.getId(), userVo.getAvatarId());
+	public DataTableRowVo updateIndividualMessage(UserVo userVo, 
+			@RequestParam(name = "newPassword", required = false)String newPassword,
+			HttpServletResponse response) throws IOException{
+		if(!StringUtil.isBlank(newPassword)) userVo.setPassword(newPassword);
 		return userService.update(userVo);
 	}
 }
