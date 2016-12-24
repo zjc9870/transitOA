b7b53a7fe742419a7828e1e72fb6be0d966fe5ff
LonelyObserver <478528180@qq.com>
2016-12-24 19:37:25 +0800

    个人设置

--- src/main/resources/static/js/admin/personalCon/personalCon.js
+++ src/main/resources/static/js/admin/personalCon/personalCon.js
@@ -2,12 +2,15 @@ $('#password1').blur(function () {
     if(this.value != $('#password2').val()) {
         $('#passwordTips').text('密码与上一步输入的密码不一致');
         $('#passwordTips').css('color','red');
+        $('#passwordTips').val('false');
     } else if(this.value=='' && $('#password2').val()=='') {
         $('#passwordTips').text('重复输入的密码必须和上一步输入的密码保持一致');
         $('#passwordTips').css('color','rgb(150,150,150)');
+        $('#passwordTips').val('true');
     } else {
         $('#passwordTips').text('密码正确');
         $('#passwordTips').css('color','rgb(67,175,60)');
+        $('#passwordTips').val('true');
     }
 });
 
@@ -15,12 +18,15 @@ $('#password2').blur(function () {
     if(this.value != $('#password1').val()) {
         $('#passwordTips').text('密码与上一步输入的密码不一致');
         $('#passwordTips').css('color','red');
+        $('#passwordTips').val('false');
     } else if(this.value=='' && $('#password1').val()=='') {
         $('#passwordTips').text('重复输入的密码必须和上一步输入的密码保持一致');
         $('#passwordTips').css('color','rgb(150,150,150)');
+        $('#passwordTips').val('true');
     } else {
         $('#passwordTips').text('密码正确');
         $('#passwordTips').css('color','rgb(67,175,60)');
+        $('#passwordTips').val('true');
     }
 });
 
@@ -31,7 +37,12 @@ $('#reset').click(function () {
 });
 
 $('#saveCon').click(function () {
-    AjaxTool.post('personalCon/updateIndividualMessage', $('#c_person_form').serialize(),function () {
-        alert('保存成功!');
-    });
+    var userId = $('#userId').val();
+    if($('#passwordTips').val() == 'false') {
+        alert('密码不一致!');
+    } else {
+        AjaxTool.post('personalCon/updateIndividualMessage', $('#c_person_form').serialize()+"&id="+userId,function (data) {
+        alert(data.message);
+        });
+    }
 });
