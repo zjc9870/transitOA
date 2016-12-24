c2466333b9bfe57d034e5760cc8e61d9f2fb5d9d
LonelyObserver <478528180@qq.com>
2016-12-24 14:19:24 +0800

    合同页面

--- src/main/resources/static/js/admin/contract/c_find.js
+++ src/main/resources/static/js/admin/contract/c_find.js
@@ -10,10 +10,17 @@ $('#searchCon').click(function () {
                 str += "<td><div>"+cons[i].date+"</div><div>"+ cons[i].time +"</div></td>";
                 str += "<td>"+cons[i].userName+"</td>";
                 str += "<td>"+cons[i].htshzt+"</td>";
-                str += "<td><div onclick='seeApprove(\""+cons[i].id +"\")'>查看</div></td>";
+                str += "<td><div onclick='seeConDetail(\""+cons[i].id +"\")'>查看</div></td>";
                 str += "</tr>";
             }
-            $('#c-find-tbody').html(str);
+            $('#c-approve-tbody').html(str);
         }
     });
 });
+
+
+function seeConDetail(id) {
+    AjaxTool.html('contract/ssxq',{id:id},function (html) {
+        $('.portlet-body').html(html);
+    });
+}
---
+++ src/main/resources/static/js/admin/contract/c_findDetail.js
@@ -0,0 +1,41 @@
+(function () {
+    //附件查看
+    var n = 1;
+    $('#fjck').click(
+        function () {
+            if(n%2==1) {
+                var attachList = JSON.parse($('#attachList').val());
+                for (var i = 0; i < attachList.length; i++) {
+                    var li = document.createElement('li');
+                    var div = document.createElement('div');
+                    div.innerHTML = attachList[i].name;
+                    div.setAttribute('style', 'cursor:pointer;');
+                    div.setAttribute('class', 'attachment');
+                    li.appendChild(div);
+                    li.setAttribute('class', 'attList')
+                    this.parentNode.appendChild(li);
+                    div.id = attachList[i].id;              //将变量保存给对象,避免循环闭包
+                    div.onclick = function () {
+                        window.location = "attachment/download?id=" + this.id;
+                    }
+                }
+            }
+            else {
+                $('.attList').hide();
+            }
+            n += 1;
+        }
+    );
+
+
+
+    $('#back').click(function () {
+        AjaxTool.getHtml('contract/htcx',function (html) {
+            $('.page-content').html(html);
+        });
+    });
+
+
+})();
+
+
