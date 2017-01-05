08b733a2615dd9686ec37d6916c12d34ea4aaaf1
LonelyObserver <478528180@qq.com>
2017-01-05 17:18:49 +0800

    表单验证

--- src/main/resources/static/js/admin/personalCon/personalCon.js
+++ src/main/resources/static/js/admin/personalCon/personalCon.js
@@ -1,48 +1,52 @@
-$('#password1').blur(function () {
-    if(this.value != $('#password2').val()) {
-        $('#passwordTips').text('密码与上一步输入的密码不一致');
-        $('#passwordTips').css('color','red');
-        $('#passwordTips').val('false');
-    } else if(this.value=='' && $('#password2').val()=='') {
-        $('#passwordTips').text('重复输入的密码必须和上一步输入的密码保持一致');
-        $('#passwordTips').css('color','rgb(150,150,150)');
-        $('#passwordTips').val('true');
-    } else {
-        $('#passwordTips').text('密码正确');
-        $('#passwordTips').css('color','rgb(67,175,60)');
-        $('#passwordTips').val('true');
-    }
+$().ready(function () {
+    var validator = $('#c_person_form').validate({
+        errorElement: 'span', //default input error message container
+        errorClass: 'error-tips', // default input error message class
+        rules: {
+            newPassword: {
+                equalTo: "#password1"
+            },
+            phoneNumber1: {
+                phoneUS: true
+            },
+            phoneNumber2: {
+                phoneUS: true
+            },
+            email: {
+                email: true
+            },
+        },
+
+        messages: {
+            newPassword: {
+                equalTo: "两次输入密码不一致"
+            },
+            phoneNumber1: {
+                phoneUS: "请输入正确的手机号"
+            },
+            phoneNumber2: {
+                phoneUS: "请输入正确的手机号"
+            },
+            email: {
+                email: "请输入正确的邮箱"
+            }
+        }
+    });
+
+    $('#reset').click(function () {
+        $("input[disabled!='disabled']").val('');
+        $("input[name='sex']").attr('checked',false);
+    });
+    $('#saveCon').click(function () {
+        var userId = $('#userId').val();
+        if(validator.form()) {
+            AjaxTool.post('personalCon/updateIndividualMessage', $('#c_person_form').serialize()+"&id="+userId,function (data) {
+                alert(data.message);
+            })
+        } else {
+            alert("保存失败");
+        }
+    });
 });
 
-$('#password2').blur(function () {
-    if(this.value != $('#password1').val()) {
-        $('#passwordTips').text('密码与上一步输入的密码不一致');
-        $('#passwordTips').css('color','red');
-        $('#passwordTips').val('false');
-    } else if(this.value=='' && $('#password1').val()=='') {
-        $('#passwordTips').text('重复输入的密码必须和上一步输入的密码保持一致');
-        $('#passwordTips').css('color','rgb(150,150,150)');
-        $('#passwordTips').val('true');
-    } else {
-        $('#passwordTips').text('密码正确');
-        $('#passwordTips').css('color','rgb(67,175,60)');
-        $('#passwordTips').val('true');
-    }
-});
 
-
-$('#reset').click(function () {
-    $("input[disabled!='disabled']").val('');
-    $("input[name='sex']").attr('checked',false);
-});
-
-$('#saveCon').click(function () {
-    var userId = $('#userId').val();
-    if($('#passwordTips').val() == 'false') {
-        alert('密码不一致!');
-    } else {
-        AjaxTool.post('personalCon/updateIndividualMessage', $('#c_person_form').serialize()+"&id="+userId,function (data) {
-        alert(data.message);
-        });
-    }
-});
