064a6874ff9250bf177eda3496bc098aeb9a8f79
LonelyObserver <478528180@qq.com>
2017-01-09 09:53:43 +0800

    密码登陆，日历，提交提示、字符长度等修改

--- src/main/resources/static/js/admin/contract/c_backfillDetail.js
+++ src/main/resources/static/js/admin/contract/c_backfillDetail.js
@@ -1,50 +1,80 @@
-function submitForm() {
-    AjaxTool.post('contract/bhht', $('#c_backfillForm').serialize(), function (data) {
-            alert(data.message);
-            toBhht();
-        }
-    )
-}
+$().ready(function () {
+    var validator = $('#c_backfillForm').validate({
+        errorElement: 'span',
+        errorClass: 'error-tips',
+        rules: {
+            bh: {
+                required: true,
+                isBlank: '',
+                maxlength: 20
+            }
+        },
 
-function toBhht() {
-    AjaxTool.getHtml('contract/getBhhtList',function (html) {
-        $('.page-content').html(html);
-    });
-}
-
-
-$('#back').click(function () {
-    var tabId = $('#back').data('tabId');
-    AjaxTool.getHtml('contract/getBhhtList',function (html) {
-        $('.page-content').html(html);
-        $('#'+tabId).trigger("click");
-    });
-});
-
-//附件查看
-var n = 1;
-$('#fjck').click(
-    function () {
-        if(n%2==1) {
-            var attachList = JSON.parse($('#attachList').val());
-            for (var i = 0; i < attachList.length; i++) {
-                var li = document.createElement('li');
-                var div = document.createElement('div');
-                div.innerHTML = attachList[i].name;
-                div.setAttribute('style', 'cursor:pointer;');
-                div.setAttribute('class', 'attachment');
-                li.appendChild(div);
-                li.setAttribute('class', 'attList')
-                this.parentNode.appendChild(li);
-                div.id = attachList[i].id;              //将变量保存给对象,避免循环闭包
-                div.onclick = function () {
-                    window.location = "attachment/download?id=" + this.id;
-                }
+        messages: {
+            bh: {
+                required: "请输入合同编号",
+                isBlank: "请输入合同编号",
+                maxlength: "合同编号至多20个字"
             }
         }
-        else {
-            $('.attList').hide();
+    });
+
+    $('#submit').click(function () {
+        if(validator.form()) {
+            AjaxTool.post('contract/bhht', $('#c_backfillForm').serialize(), function (data) {
+                    alert(data.message);
+                    if(data.success) {
+                        toBhht();
+                    }
+                }
+            )
+        } else {
+           alert('提交失败!');
         }
-        n += 1;
+    });
+
+    function toBhht() {
+        AjaxTool.getHtml('contract/getBhhtList',function (html) {
+            $('.page-content').html(html);
+        });
     }
-);
+
+
+    $('#back').click(function () {
+        var tabId = $('#back').data('tabId');
+        AjaxTool.getHtml('contract/getBhhtList',function (html) {
+            $('.page-content').html(html);
+            setTimeout(function () {
+                $('#'+tabId).trigger("click");
+            },100);
+        });
+    });
+
+//附件查看
+    var n = 1;
+    $('#fjck').click(
+        function () {
+            if(n%2==1) {
+                var attachList = JSON.parse($('#attachList').val());
+                for (var i = 0; i < attachList.length; i++) {
+                    var li = document.createElement('li');
+                    var div = document.createElement('div');
+                    div.innerHTML = attachList[i].name;
+                    div.setAttribute('style', 'cursor:pointer;');
+                    div.setAttribute('class', 'attachment');
+                    li.appendChild(div);
+                    li.setAttribute('class', 'attList')
+                    this.parentNode.appendChild(li);
+                    div.id = attachList[i].id;              //将变量保存给对象,避免循环闭包
+                    div.onclick = function () {
+                        window.location = "attachment/download?id=" + this.id;
+                    }
+                }
+            }
+            else {
+                $('.attList').hide();
+            }
+            n += 1;
+        }
+    );
+});
