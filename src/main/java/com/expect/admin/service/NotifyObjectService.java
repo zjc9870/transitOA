e481bbcd290ea7ba2e768c40f4dca2fad5151d6b
qifeng <643437475@qq.com>
2017-05-19 11:25:22 +0800

    删除pingyin4j

--- src/main/java/com/expect/admin/service/NotifyObjectService.java
+++ src/main/java/com/expect/admin/service/NotifyObjectService.java
@@ -14,11 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 
-import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
-import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
-import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 
 /**
  * Created by qifeng on 17/5/10.
