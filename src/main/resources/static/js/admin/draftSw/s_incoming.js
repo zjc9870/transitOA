ef60d90f13e22e5a596b3bcfa9dc7f55d017982c
LonelyObserver <478528180@qq.com>
2017-05-22 19:12:16 +0800

    收文相关

--- src/main/resources/static/js/admin/draftSw/s_incoming.js
+++ src/main/resources/static/js/admin/draftSw/s_incoming.js
@@ -3,29 +3,29 @@ $(document).ready(function () {
     $('#uploadFile').click(function () {
         DatatableTool.modalShow("#upload-modal", "#fileUploadForm");
         var uploader = $("#fileUploadForm").FileUpload({
-            url: "contract/uploadContractAttachment",
+            url: "draftSw/uploadDraftSwAttachment",
             isMultiFile: true,
-        });
-        uploader.done(function(data) {
-            ids.push(data.result.id);
-            var li = document.createElement('li');
-            var span = document.createElement('span');
-            span.innerHTML = 'x';
-            span.setAttribute('style','margin-left:10px;color:red;font-weight:bold;cursor:pointer');
-            span.setAttribute('class','delete');
-            span.setAttribute('id',data.result.id);
-            li.innerHTML = data.result.name;
-            li.appendChild(span);
-            $('#fjlb').append(li);
+    });
+    uploader.done(function(data) {
+        ids.push(data.result.id);
+        var li = document.createElement('li');
+        var span = document.createElement('span');
+        span.innerHTML = 'x';
+        span.setAttribute('style','margin-left:10px;color:red;font-weight:bold;cursor:pointer');
+        span.setAttribute('class','delete');
+        span.setAttribute('id',data.result.id);
+        li.innerHTML = data.result.name;
+        li.appendChild(span);
+        $('#fjlb').append(li);
 
-            var deletes = document.getElementsByClassName('delete');
-            for(var i=0;i<deletes.length;i++) {
-                deletes[i].onclick = function () {
-                    ids.splice(ids.indexOf(this.id),1);
-                    this.parentNode.setAttribute('style','display:none;');
-                }
+        var deletes = document.getElementsByClassName('delete');
+        for(var i=0;i<deletes.length;i++) {
+            deletes[i].onclick = function () {
+                ids.splice(ids.indexOf(this.id),1);
+                this.parentNode.setAttribute('style','display:none;');
             }
-        });
+        }
+    });
     });
 
 
@@ -41,7 +41,8 @@ $(document).ready(function () {
                 }
             }
             if(validator.form()) {
-                AjaxTool.post('draftSw/saveSw', $('#s_incoming_form').serialize()+"&bczl="+this.id+"&fileId="+ids, function (data) {
+                var ldId = document.getElementsByName('ldId')[0].value;
+                AjaxTool.post('draftSw/saveSw', $('#s_incoming_form').serialize()+"&ldId="+ldId+"&bczl="+this.id+"&fileId="+ids, function (data) {
                         alert(data.message);
                         toSqjl();
                     }
@@ -49,7 +50,6 @@ $(document).ready(function () {
             }
         };
     }
-
     function toSqjl() {
         AjaxTool.getHtml('draftSw/swRecord',function (html) {
             $('.page-content').html(html);
